164d67c758e1539924598d8e20c7794154d7497c
datqu <datiobashvili1@gmail.com>
2024-11-26 23:20:39 +0400

    created fields page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",'./about.html'],
+  content: ["./index.html",'./about.html','fields.html'],
   theme: {
     extend: {
       screens: {
