a4321e310c73dc6f225373761a0f249160fbe892
datqu <datiobashvili1@gmail.com>
2025-01-07 19:08:53 +0400

    finished version

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         sans: ["Roboto", "Arial", "sans-serif"],
         primary: ["ALK Rounded Mtav Med", "serif"],
         secondary: ["ArchyEDT-Bold", "serif"],
+        primaryRus: ["Overpass", "serif"],
+        secondaryRus: ["Play", "serif"],
+        primaryEng: ["Kanit", "serif"],
+        secondaryEng: ["Ubuntu", "serif"],
       },
       colors: {
         firstRed: "#fe0036",
